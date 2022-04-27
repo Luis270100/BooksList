d19116373d0d53aaf6c2f68a5f0c88516ae03829
Luis270100 <luisarthe27@gmail.com>
2022-04-27 17:00:23 -0500

    Initial Commit

---
+++ books/booksApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksApp.swift
+//  books
+//
+//  Created by Luis Arturo Hernandez on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct booksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
